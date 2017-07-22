ca014053f24d34e086c5181695f90121720b5b3e
Shreepad Shukla <shreepads@gmail.com>
2017-07-23 01:44:03 +0530

    Store only hashtagged messages by hashtag. On summarise command generate a HTML summary, store it on S3 and respond with a message that includes the S3 URL

--- scribrbot.py
+++ scribrbot.py
@@ -10,20 +10,52 @@ ScribrBot main module. Handles the webhook events from Telegram.
 
 
 from __future__ import print_function
-import os, json
+import os, json, urllib
+from datetime import datetime
 
-import boto3
+import boto3, requests
+
+from boto3.dynamodb.conditions import Key, Attr
+from jinja2 import Template
+
 
 # Setup outside main event handler
 
-token = os.environ['TelegramBotToken']
-print('Token: %s' % token) 
-    
+# Get environ variables
+telegram_token = os.environ['TelegramBotToken']
+print('Token: %s' % telegram_token) 
 msg_table_name =  os.environ['MessageTableName']
 print('Msg table name: %s' % msg_table_name) 
+s3_bucket_name = os.environ['S3BucketName']
+print('S3 bucket name: %s' % s3_bucket_name) 
 
+# Get Dyname DB table and S3 Bucket resources
 dynamodb_res = boto3.resource('dynamodb')    
 message_table = dynamodb_res.Table(msg_table_name)
+s3 = boto3.resource('s3')
+s3bucket = s3.Bucket(s3_bucket_name)
+
+
+# Setup Jinja2 template
+templatestr = """
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <table>
+                     {% for item in items %}
+                     <tr>
+                          <td> {{ item['UserFirstName'] }} </td>
+                          <td> {{ item['Text'] }}         </td>
+                          {% set ctime = datetime.fromtimestamp(int(item['UnixTimestamp'])).ctime() %}
+                          <td> {{ ctime }} </td>
+                     </tr>
+                     {% endfor %}
+                </table>
+            </body>
+        </html>"""
+template = Template(templatestr)
+
+
 
 def lambda_handler(event, context):
     """
@@ -49,6 +81,13 @@ def lambda_handler(event, context):
     if 'entities' not in event['message']:
         print('No entities element in message')
         return 'Nothing for me to do here'
+    
+    
+    # Figure out if a command has been issued, execute the first one and return
+    for entity in event['message']['entities']:
+        if entity['type'] == 'bot_command':
+            executecommand(event['message'], entity)
+            return 'Jawohl mein fuhrer!'
     
     
     # Get set of hashtags from message and return if none
@@ -103,4 +142,94 @@ def getHashtagsFromMessage(message):
     print('Returning hashtags: %s' % hashtags)
     
     return hashtags
+
+
+def executecommand(message, bot_command_entity):
+    """
+    Executes the bot command contained in the Telegram message.
+    """
+    
+    responsemsg = ''
+    chat_id = message['chat']['id']
+    text = message['text']
+    offset = int(bot_command_entity['offset'])
+    length = int(bot_command_entity['length'])
+    
+    bot_command = text[offset + 1 : offset + length]  
+    
+    print('Handling command %s for message text %s' % (bot_command, text))
+    
+    if bot_command == 'summ':
+        hashtags = getHashtagsFromMessage(message)
+
+        if not hashtags:
+            print('No hashtags in summ message text: %s' % text)
+        else:
+            summ_s3_url = generatesummary(chat_id, hashtags)
+            responsemsg = 'Summary URL: {}'.format(summ_s3_url)
+    
+    if not responsemsg:
+        responsemsg = 'Sorry I cannot handle that command'
         
+    sendresponse(chat_id, responsemsg)
+    return
+
+
+
+def generatesummary(chat_id, hashtags):
+    """
+    Generates a summary for hashtags on the chat_id, stores on S3 and returns
+    S3 url
+    """
+    
+    hashtag = hashtags.pop()
+    
+    # Scan the table for all messages that contain the given hashtag
+    scanresponse = message_table.scan(
+        FilterExpression=Attr('Hashtags').contains(hashtag) & Attr('ChatId').eq(chat_id)
+    )
+    
+    
+    # Generate the HTML summary of the messages
+    htmlsummary = template.render(
+        items=scanresponse['Items'], 
+        datetime=datetime, int=int
+    )
+    
+    # Store the summary on S3
+    s3_key = 'summaries/{0}/{1}.html'.format(chat_id, hashtag)
+    s3_object = s3bucket.put_object(
+        Key=s3_key, 
+        Body=htmlsummary.encode('utf8'),
+        ACL='public-read',
+        ContentType='text/html; charset=utf-8'
+    )
+    
+    s3_url = 'https://{0}.s3.amazonaws.com/{1}'.format(s3_bucket_name, s3_key)
+    
+    return s3_url
+    
+
+def sendresponse(chat_id, responsemsg):
+    """
+    Sends the responsemsg on the chat_id.
+    """
+    
+    sendresponse_url = \
+        "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}".format(
+            telegram_token,
+            chat_id,
+            urllib.quote(responsemsg.encode('utf8'))
+        )
+        
+    response = requests.get(sendresponse_url)
+    
+    if response.status_code == 200:
+        print("Message %s sent on chat_id %s" % (responsemsg, chat_id) )
+    else:
+        print("Message %s NOT sent on chat_id %s" % (responsemsg, chat_id) )
+        print("Response: %r" % response)
+        
+    return
+    
+    
